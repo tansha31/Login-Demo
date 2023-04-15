1b26db29d0bc759f920545200c4eaab7e412def1
Tanmay Sharma <35055243+tansha31@users.noreply.github.com>
2023-04-15 13:29:53 +0530

    added dark ui

--- index.js
+++ index.js
@@ -17,7 +17,19 @@ for (let i = 0; i < document.querySelectorAll(".form__input").length; i++) {
 document.querySelector(".theme-icon").addEventListener("click", function () {
     if (this.getAttribute("class") === "fa-regular fa-sun fa-xl theme-icon") {
         this.setAttribute("class", "fa-regular fa-moon fa-xl theme-icon");
+        document.querySelector(".navbar h1 i").style.color = "#fff";
+        document.querySelector(".navbar").classList.add("theme-dark-nav");
+        document.querySelector(".form").classList.add("theme-dark-form");
+        document
+            .querySelector(".container")
+            .classList.add("theme-dark-container");
     } else {
         this.setAttribute("class", "fa-regular fa-sun fa-xl theme-icon");
+        document.querySelector(".navbar h1 i").style.color = "#43a4ee";
+        document.querySelector(".navbar").classList.remove("theme-dark-nav");
+        document.querySelector(".form").classList.remove("theme-dark-form");
+        document
+            .querySelector(".container")
+            .classList.remove("theme-dark-container");
     }
 });
